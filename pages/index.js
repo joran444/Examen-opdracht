07bad403fdc682e17bb55f500e7844a137dd2271
Joran444 <j.hoegen@hq-online.nl>
2023-03-21 09:42:54 +0100

    Fix for building error
    This is/should be the fix to build the website in netlify

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,59 @@
 // at the top of your component file
 
-import { fetchEntries } from '@utils/contentfulPosts'
+import { fetchEntries } from 'utils/contentfulPosts'
 import Post from '@components/Post'
 
-// inside your component markup, pull `posts` from props
+export default function Home({ posts }) {
+  return (
+    <div className="container">
+        <title>Next + Contentful Starter</title>
+        <link rel="icon" href="/favicon.ico" />
 
-<div className="posts">
-  {posts.map((p) => {
-    return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-  })}
-</div>
+      <main>
+        <div className="post">
+            {posts.map((p) => {
+              return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+            })}
+        </div>
+      </main>
 
-// at the bottom of your component file
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export async function getStaticProps() {
   const res = await fetchEntries()
